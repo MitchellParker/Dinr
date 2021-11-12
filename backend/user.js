577428c99ca11864b8a0dc5198f949de9e81b2d3
grewalanmol <gerwalanmol21@gmail.com>
2021-11-12 03:25:37 -0800

    starting schema for users

---
+++ backend/user.js
@@ -0,0 +1,15 @@
+const mongoose = require ('mongoose');
+
+const Schema = mongoose.Schema;
+
+const SupporterSchema = new Schema({
+    nickname: String,//need some unique identifier to check for account duplicates   
+    password: String,
+    breakfast: String,
+    lunch: String,
+    dinner: String,
+    friendlist: Array
+    
+})
+
+module.exports = mongoose.model('supporter', SupporterSchema);
