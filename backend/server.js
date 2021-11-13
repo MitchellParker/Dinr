5f79443219fb750e285086c31714aede021859c5
Mitchell Parker <mitchellparker@ucla.edu>
2021-11-13 11:38:48 -0800

    Allow server to send frontend information

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const routes = require("./routes");
 const connectDB = require("./db");
 
@@ -12,12 +13,16 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/routes', routes)
 
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(function (req, res, next) {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+app.get("/", function (req, res) {
+  res.send("404 Error");
+});
+
 // DB Config
 connectDB();
-
-app.get("/", function (req, res) {
-  res.send("Hello World!");
-});
 
 // Listener
 app.listen(port, () => console.log(`listening on localhost: ${port}`));
