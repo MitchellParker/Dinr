642914d9aa7f18c245ded4045dd819ed5c6a4526
Mitchell Parker <mitchellparker@ucla.edu>
2021-11-12 17:49:43 -0800

    Added html form that gets username and password

--- dinr/src/pages/login.js
+++ dinr/src/pages/login.js
@@ -4,9 +4,23 @@ import './login.css';
 const LogIn = () =>{
   return (
     <body className='main'>
-        <div>
-            <p>Log In</p>
-        </div>
+      <div>
+        <h1> Welcome to Dinr! </h1>
+        <br/>
+        <form id="loginForm">
+            <label for="username">
+                Username:
+            </label>
+            <input type="text" name="username" id="username" required />
+            <br/>
+            <label for="password">
+                Password:
+            </label>
+            <input type="password" name="password" id="password" required />
+            <br/>
+            <input type="submit" value="Login" />
+        </form>
+      </div>
     </body>
   );
 }
