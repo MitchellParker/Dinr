33d49c8a53facb2c08e3791539e4ab1350b7bd2c
grewalanmol <gerwalanmol21@gmail.com>
2021-11-12 18:23:38 -0800

    db for the database

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+require("dotenv/config");
+const mongoose = require('mongoose');
+const URI = process.env.DB_URI;
+
+const connectDB = async () => {
+    await mongoose.connect(URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    })
+    console.log("MongoDB Successfully Connected!")
+}
+
+module.exports = connectDB;
