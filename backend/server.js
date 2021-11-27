af43b404dcd9c2db23efe9e0a74c07af1b095296
Mitchell Parker <mitchellparker@ucla.edu>
2021-11-26 22:05:21 -0800

    Frontend requests can be proxied to backend
    Backend is now on a different port and api calls
    now get proxied to that port. This allows the react
    app and express server to be run separately in two
    different terminals with "npm run start-client" and
    "npm run start-server" respectively. This means no
    more fiddling in the command line every time you
    want to see a change while testing.

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser');
 
 // App Config
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 
 // Middlewares
@@ -37,5 +37,3 @@ connectDB();
 
 // Listener
 app.listen(port, () => console.log(`listening on localhost: ${port}`));
-
-// can not create the full server right now as that need all the css and html files to be ready 
