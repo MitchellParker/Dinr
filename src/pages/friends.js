7d51812bfde2b708f139d75f00d59486bb132362
lucianli <lucianli02@gmail.com>
2021-11-29 17:10:52 -0800

    Created add friends search bar feature
    User must type exact nickname. Includes error messages that display if user inputs an invalid nickname or a nickname that is already in their friend list.

--- src/pages/friends.js
+++ src/pages/friends.js
@@ -4,7 +4,7 @@ import Data from "../tempmockdata/mock-data.json";
 import useAuth from '../useAuth';
 import axios from 'axios';
 
-const Friends = () =>{
+const Friends = () => {
     const [query, setQuery] = useState("");
     const [error, getError] = useState(null);
     const [breakfast, getBreakfast] = useState('');
@@ -13,10 +13,12 @@ const Friends = () =>{
     const [lunchTime, getLunchTime] = useState('');
     const [dinner, getDinner] = useState('');
     const [dinnerTime, getDinnerTime] = useState('');
+    const [input, setInput] = useState('');
+    const [message, setMessage] = useState('');
     const { user } = useAuth();
 
-    const getUserDiningChoices = () => {
-        axios.get("http://localhost:3001/fetch/?nickname=" + user)
+    const getUserData = () => {
+        axios.get('http://localhost:3001/fetch/?nickname=' + user)
         .then((response) => {
             getBreakfast(response.data.breakfast);
             getBreakfastTime(response.data.breakfastTime);
@@ -25,54 +27,116 @@ const Friends = () =>{
             getDinner(response.data.dinner);
             getDinnerTime(response.data.dinnerTime);
         })
+        .catch(error => getError(error));
+    }
+    const handleChange = (event) => {
+        setInput(event.target.value);
+    }
+    async function handleSubmit(event) {
+        setMessage('');
+        //check if user gave a valid nickname
+        let isValidNickname = true;
+        axios.get('http://localhost:3001/fetch/?nickname=' + input)
+        .then(response => {
+            if (response.data.length == 0) {
+                setMessage('Invalid nickname');
+                isValidNickname = false;
+            }
+        })
+        .then(() => {
+            if (isValidNickname) {
+                //get user's current friendlist
+                let friends = [];
+                axios.get('http://localhost:3001/fetchfriends/?nickname=' + user)
+                .then((response) => {
+                    friends = response.data;
+                })
+                .then(() => {
+                    //check if friend is already in friendlist
+                    let alreadyInList = false;
+                    for (let i = 0; i < friends.length; i++) {
+                        if (friends[i] == input) {
+                            alreadyInList = true;
+                            setMessage('Friend is already in your list');
+                            break;
+                        }
+                    }
+                    if (!alreadyInList) {
+                        //update user's friendlist with new added friend
+                        const updatedFriendList = friends.concat(input);
+                        axios.put('http://localhost:3001/updatefriends/:nickname',
+                        {
+                            nickname: user,
+                            friendlist: updatedFriendList
+                        });
+                        setMessage('Added ' + input + ' to you friend list');
+                    }
+                })
+                .catch(error => {
+                    getError(error);
+                });
+            }
+        })
         .catch(error => {
-            getError(error)
-        });
+            getError(error);
+        })
+        //clear search box
+        setInput('');
+        event.preventDefault();
     }
 
     useEffect(() => {
-        getUserDiningChoices();
-    })
+        getUserData();
+    }, []);
 
     return (
-        <div className = "friends">
-        <input placeholder="Search for Friends!" onChange={event => setQuery(event.target.value)} />
-        <div className='friends_yourChoices'>
-        <div className='friends_yourChoicesLabel'>Your Choices</div>
-            <div className='friends_mealPeriod'>
-                <h2>Breakfast</h2>
-                <p>{breakfast} at {breakfastTime}</p>
-            </div>
-            <div className='friends_mealPeriod'>
-                <h2>Lunch</h2>
-                <p>{lunch} at {lunchTime}</p>
-            </div>
-            <div className='friends_mealPeriod'>
-                <h2>Dinner</h2>
-                <p>{dinner} at {dinnerTime}</p>
-            </div> 
-        </div>
-        <div className = "friendschoice">
-        {
-        Data.filter((post) => {
-            if (query === '') {
-                return post
-            } else if (post.friend.toLowerCase().includes(query.toLowerCase())) {
-                return post
-            }
-        }).map((post, index) => {
-            return (
-                <div className = "indfriend" key={index}>
-                    <p>Friend: {post.friend}</p>
-                    <p className = "box">Breakfast: {post.breakfast}</p>
-                    <p className = "box">Lunch: {post.lunch}</p>
-                    <p className = "box">Dinner: {post.dinner}</p>
+        <div>
+            {error ? <h1>{error}</h1> : 
+            <div className = "friends">
+                <input placeholder="Search for Friends!" onChange={event => setQuery(event.target.value)} />
+                <form onSubmit={handleSubmit}>
+                    <input placeholder="Add a Friend by Nickname" value={input} onChange={handleChange} />
+                    <input type="submit" value="Add" />
+                    <p>{message}</p>
+                </form>
+                {/* <AddFriendBar user={user} friends={friendList}/> */}
+                <div className='friends_yourChoices'>
+                    <div className='friends_yourChoicesLabel'>Your Choices</div>
+                    <div className='friends_mealPeriod'>
+                        <h2>Breakfast</h2>
+                        <p>{breakfast} at {breakfastTime}</p>
+                    </div>
+                    <div className='friends_mealPeriod'>
+                        <h2>Lunch</h2>
+                        <p>{lunch} at {lunchTime}</p>
+                    </div>
+                    <div className='friends_mealPeriod'>
+                        <h2>Dinner</h2>
+                        <p>{dinner} at {dinnerTime}</p>
+                    </div> 
                 </div>
-            );
-        })}
+                <div className = "friendschoice">
+                {
+                Data.filter((post) => {
+                    if (query === '') {
+                        return post
+                    } else if (post.friend.toLowerCase().includes(query.toLowerCase())) {
+                        return post
+                    }
+                }).map((post, index) => {
+                    return (
+                        <div className = "indfriend" key={index}>
+                            <div>Friend: {post.friend}</div>
+                            <div className = "box">{post.breakfast}</div>
+                            <div className = "box">{post.lunch}</div>
+                            <div className = "box">{post.dinner}</div>
+                        </div>
+                    );
+                })}
+                </div>
+            </div>}
         </div>
-        </div>
-  );
+    );
 }
 
 export default Friends;
